4a85c3b3b0953d654673e7dddcf9888ffc4dd9fe
Stephane Nicoll <snicoll@pivotal.io>
2019-12-11 18:48:46 +0100

    Add basic sample
    That sample showcases how the generator can be used with a running app.
    
    It generates the bootstrap code in the same source tree provided that the
    application runs with the sample directory as the working directory.
    
    The generated code does not compile at the moment.

---
+++ spring-context-bootstrap-samples/spring-context-bootstrap-sample-generator/src/main/java/sample/generator/SimpleApplication.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2012-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sample.generator;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+import com.squareup.javapoet.JavaFile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.bootstrap.generator.ContextBootstrapGenerator;
+
+@SpringBootApplication
+public class SimpleApplication {
+
+	public static void main(String[] args) throws IOException {
+		ConfigurableApplicationContext context = SpringApplication.run(SimpleApplication.class, args);
+
+		JavaFile javaFile = new ContextBootstrapGenerator(context.getBeanFactory())
+				.generateBootstrapClass("sample.generator.bootstrap");
+		// In IntelliJ IDEA, make sure that "working directory" is set to $MODULE_DIR$
+		Path srcDirectory = FileSystems.getDefault().getPath(".").resolve("src/main/java");
+		javaFile.writeTo(srcDirectory);
+	}
+
+}
